ac89144e3acd55484ab498797ecd2210a76a2f83
Pouya Kary <k@karyfoundation.org>
2017-05-01 23:44:43 +0430

    messages are now much more KF

--- src/messages.js
+++ src/messages.js
@@ -1,10 +1,9 @@
 exports.messages = {
-	admin: 'Run VS Code with admin privileges so the changes can be applied.',
-	enabled: 'Custom CSS and JS enabled. Restart to take effect. If Code complains about it is corrupted, CLICK DON\'T SHOW AGAIN. See README for more detail.',
-	disabled: 'Custom CSS and JS disabled and reverted to default. Restart to take effect.',
-	already_disabled: 'Custom CSS and JS already disabled.',
+	admin: 'Run Visual Studio Code with admin privileges so the changes can be applied.',
+	enabled: 'Kary Foundation Light UI is enabled. Restart to take effect. If Code complains about it is corrupted, CLICK DON\'T SHOW AGAIN. See README for more detail.',
+	disabled: 'Kary Foundation Light UI disabled and reverted to default. Restart to take effect.',
+	already_disabled: 'Kary Foundation Light UI already disabled.',
 	smthingwrong: 'Something went wrong: ',
 	restartIde: 'Restart Visual Studio Code',
-	notfound: 'Custom CSS and JS not found.',
-	notconfigured: 'Custom CSS and JS path not configured. Please set "vscode_custom_css.imports" in your user settings.'
+	notfound: 'Kary Foundation Light UI not found.',
 };
